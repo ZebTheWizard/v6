6ce8e20fde1eeb108019251fbdbebe94ac0b57da
criticalthewizard <burroughszeb@me.com>
2018-07-10 09:41:51 -0500

    no async in setup

--- auth/setup/github.js
+++ auth/setup/github.js
@@ -6,8 +6,7 @@ module.exports = new Strategy({
   clientID: config.get('github.key'),
   clientSecret: config.get('github.secret'),
   callbackURL: config.get("github.callback")
-}, async function (token, secret, profile, done) {
-  console.log(profile);
+}, function (token, secret, profile, done) {
   User.findOne({ githubID: profile.id }, (err, user) => {
     if (user) return done(null, user)
 
--- auth/setup/twitter.js
+++ auth/setup/twitter.js
@@ -6,7 +6,7 @@ module.exports = new Strategy({
   consumerKey: config.get('twitter.key'),
   consumerSecret: config.get('twitter.secret'),
   callbackURL: config.get("twitter.callback")
-}, async function (token, secret, profile, done) {
+}, function (token, secret, profile, done) {
   User.findOne({ twitterID: profile.id }, (err, user) => {
     if (user) return done(null, user)
 
