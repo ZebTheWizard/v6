762a837ee588733262893c1c1840d2ebd1470abb
criticalthewizard <burroughszeb@me.com>
2018-07-09 20:15:10 -0500

    config

--- config/production.js
+++ config/production.js
@@ -5,6 +5,11 @@ module.exports = {
     "secret": process.env.TWITTER_SECRET,
     "callback": process.env.TWITTER_CALLBACK
   },
+  "github": {
+    "key": process.env.GITHUB_KEY,
+    "secret": process.env.GITHUB_SECRET,
+    "callback": process.env.GITHUB_CALLBACK
+  },
   "MONGODB_URI": process.env.MONGODB_URI,
   "title": process.env.APP_TITLE || "IOS Haven"
 }
