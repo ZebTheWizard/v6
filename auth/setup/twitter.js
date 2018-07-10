8a5ecdf3856346f5ececb9a2ff9f9ce14791c0b4
criticalthewizard <burroughszeb@me.com>
2018-07-09 19:33:53 -0500

    callback env

--- auth/setup/twitter.js
+++ auth/setup/twitter.js
@@ -5,7 +5,7 @@ const { User } = require('../../models')
 module.exports = new Strategy({
   consumerKey: config.get('twitter.key'),
   consumerSecret: config.get('twitter.secret'),
-  callbackURL: '/login/twitter/return'
+  callbackURL: config.get("twitter.callback")
 }, async function (token, secret, profile, done) {
   User.findOne({ twitterID: profile.id }, (err, user) => {
     if (user) return done(null, user)
--- config/production.js
+++ config/production.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": process.env.NODE_ENV,
   "twitter": {
     "key": process.env.TWITTER_KEY,
-    "secret": process.env.TWITTER_SECRET
+    "secret": process.env.TWITTER_SECRET,
+    "callback": process.env.TWITTER_CALLBACK
   },
   "MONGODB_URI": process.env.MONGODB_URI,
   "title": process.env.APP_TITLE || "IOS Haven"
