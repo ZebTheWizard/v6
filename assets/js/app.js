431f6e33b6d50f69796e8c38a34898db4d94e558
criticalthewizard <burroughszeb@me.com>
2018-07-15 09:53:07 -0500

    scroll snapping

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
 require('./bootstrap')
 
+$.fn.oldScroll = $.fn.scroll
+
+$.fn.scroll = function (done) {
+  this.oldScroll(function (e) {
+    e.target.oldScrollLeft = e.target.oldScrollLeft || 0
+    e.target.oldScrollTop = e.target.oldScrollTop || 0
+
+    if (e.target.oldScrollLeft < $(this).scrollLeft()) e.target.scrollDirection = 'right'
+    else if (e.target.oldScrollLeft > $(this).scrollLeft()) e.target.scrollDirection = 'left'
+    else if (e.target.oldScrollTop < $(this).scrollTop()) e.target.scrollDirection = 'down'
+    else if (e.target.oldScrollTop > $(this).scrollTop()) e.target.scrollDirection = 'up'
+
+    done(e)
+    e.target.oldScrollLeft = $(this).scrollLeft()
+    e.target.oldScrollTop = $(this).scrollTop()
+  })
+}
+
 $(window).scroll(function (e) {
-  console.log($(window).scrollTop());
   if ($(window).scrollTop() > 45) {
     $('.page-title-fixed').addClass('visible')
   } else {
@@ -14,3 +31,44 @@ $(window).scroll(function (e) {
     $('.page-title-fixed').removeClass('has-border')
   }
 })
+
+// $('.item-slider').scroll(function (e) {
+//   e.target.oldScrollLeft = e.target.oldScrollLeft || 0
+//   var direction =
+//   e.target.oldScrollLeft = $(this).scrollLeft()
+// })
+
+// $('.item-slider.sliding').scroll(function (e) {
+//   e.preventDefault()
+//   return false
+// })
+
+// $('.item-slider').scroll(function (e) {
+//
+//   let scrollAmount = 0
+//   if (e.target.scrollDirection === 'right') {
+//     scrollAmount = $(e.target).width()
+//   } else if (e.target.scrollDirection === 'left') {
+//     scrollAmount = $(e.target).width() * -1
+//   }
+//   console.log(scrollAmount);
+//   $('.item-slider').addClass('sliding').animate({
+//     scrollLeft: scrollAmount
+//   }, 800, 'swing', function () {
+//     $(this).removeClass('sliding')
+//   })
+//   // e.target.oldScrollLeft = e.target.oldScrollLeft || 0
+//   // var direction =
+//   // e.target.oldScrollLeft = $(this).scrollLeft()
+// })
+
+// (function () {
+//   let lastStop = 0
+//   $('.item-slider').not('.scrolling').scroll(_.debounce(function(){
+//       console.log('scrolling');
+//   }, 150, { 'leading': true, 'trailing': false }));
+//   $('.item-slider').not('.scrolling').scroll(_.debounce(function(){
+//       console.log('stopped');
+//       lastStop =
+//   }, 150));
+// })()
