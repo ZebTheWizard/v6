22a3511756f3cc8b224506d061b6c1b3ecc29b10
criticalthewizard <burroughszeb@me.com>
2018-07-15 21:38:24 -0500

    snap-scroll finally works on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,29 @@
 require('./bootstrap')
 
+$('*').on('touchstart', function (e) {
+  var el = e.currentTarget
+  el.swipe = { up: false, down: false, left: false, right: false}
+  el.tsY = e.originalEvent.touches[0].clientY
+  el.tsX = e.originalEvent.touches[0].clientX
+})
+$('*').on('touchmove', function (e) {
+  var el = e.currentTarget
+  el.teY = e.originalEvent.touches[0].clientY
+  el.teX = e.originalEvent.touches[0].clientX
+
+  el.swipe.left = (el.tsX > el.teX) ? true : false
+  el.swipe.right = (el.tsX < el.teX) ? true : false
+  // el.swipe.down = (el.tsY > el.teY) ? true : false
+  // el.swipe.up = (el.tsY < el.teY) ? true : false
+
+  if (el.tsX > el.teX) el.swipeDirection = 'left'
+  else if (el.tsX < el.teX) el.swipeDirection = 'right'
+  // else if (el.tsY > el.teY) el.swipeDirection = 'down'
+  // else if (el.tsY < el.teY) el.swipeDirection = 'up'
+
+})
+
+
 $(window).scroll(function (e) {
   if ($(window).scrollTop() > 45) {
     $('.page-title-fixed').addClass('visible')
@@ -12,21 +36,57 @@ $(window).scroll(function (e) {
   } else {
     $('.page-title-fixed').removeClass('has-border')
   }
+
+  window.scrolling = true
 })
 
 
-$('.item-slider').on('wheel', function (e) {
-  if (e.currentTarget.isScrolling) return
-  let scrollAmount = 0
-  if (e.originalEvent.deltaX > 0) {
-    scrollAmount = '+=' + $(e.currentTarget).width()
-  } else if (e.originalEvent.deltaX < 0) {
-    scrollAmount = '-=' + $(e.currentTarget).width()
-  }
-  e.currentTarget.isScrolling = true
-  $(this).animate({
-    scrollLeft: scrollAmount
-  }, 400, 'swing', function () {
-    e.currentTarget.isScrolling = false
-  })
-})
+// $('.item-slider').on('wheel', function (e) {
+//   if (e.currentTarget.isScrolling) return
+//   let scrollAmount = 0
+//   if (e.originalEvent.deltaX > 0) {
+//     scrollAmount = '+=' + $(e.currentTarget).width()
+//   } else if (e.originalEvent.deltaX < 0) {
+//     scrollAmount = '-=' + $(e.currentTarget).width()
+//   }
+//   e.currentTarget.isScrolling = true
+//   $(this).animate({
+//     scrollLeft: scrollAmount
+//   }, 400, 'swing', function () {
+//     e.currentTarget.isScrolling = false
+//   })
+// })
+
+// $('.item-slider').swipe({
+//   swipe (e, direction) {
+//     let el = $(this).get()[0]
+//     if (el.isScrolling) return
+//     let scrollAmount = 0
+//     if (direction === 'right') scrollAmount = '-=' + $(this).width()
+//     else if (direction === 'left') scrollAmount = '+=' + $(this).width()
+//     el.isScrolling = true
+//     $(this).animate({
+//       scrollLeft: scrollAmount
+//     }, 400, 'swing', function () {
+//       el.isScrolling = false
+//     })
+//   }
+// })
+
+// $('.item-slider').on('touchmove', function (e) {
+//       let el = e.currentTarget
+//       // if(el.swipe.down || el.swipe.up) return
+//       if (el.isScrolling) return
+//       e.preventDefault()
+//       let scrollAmount = 0
+//       // if (el.swipe.left || el.swipe.right) e.preventDefault()
+//
+//       if (el.swipe.right) scrollAmount = '-=' + $(el).width()
+//       else if (el.swipe.left) scrollAmount = '+=' + $(el).width()
+//       el.isScrolling = true
+//       $(el).animate({
+//         scrollLeft: scrollAmount
+//       }, 400, 'swing', function () {
+//         el.isScrolling = false
+//       })
+// })
--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,14 +1,18 @@
 import scrollSnapPolyfill from 'css-scroll-snap-polyfill'
 
+
 window._ = require('lodash');
 window.Popper = require('popper.js').default;
 
 try {
     window.$ = window.jQuery = require('jquery');
     require('bootstrap');
+    require('jquery-touchswipe')
 } catch (e) {}
 
 $(document).ready(function () {
+  scrollSnapPolyfill()
+
   $('.carousel').carousel({
     interval: false
   })
