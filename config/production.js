0dc25f7f3e5715828f85cb9f6cdde56dcde9ef5e
criticalthewizard <burroughszeb@me.com>
2018-08-03 22:19:49 -0500

    updated production config

--- config/production.js
+++ config/production.js
@@ -10,6 +10,17 @@ module.exports = {
     "secret": process.env.GITHUB_SECRET,
     "callback": process.env.GITHUB_CALLBACK
   },
+  "dropbox": {
+    "key": process.env.DROPBOX_KEY,
+    "secret": process.env.DROPBOX_SECRET,
+    "oauth": process.env.DROPBOX_OAUTH
+  },
+  "s3": {
+    "key": process.env.S3_KEY,
+    "secret": process.env.S3_SECRET,
+    "region": process.env.S3_REGION,
+    "bucket": process.env.S3_BUCKET
+  },
   "MONGODB_URI": process.env.MONGO_URL || process.env.MONGODB_URI,
   "title": process.env.APP_TITLE || "IOS Haven"
 }
