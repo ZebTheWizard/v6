d4befe297c4b98f637083c038d3adb53c50b4f0c
criticalthewizard <burroughszeb@me.com>
2018-08-06 13:29:44 -0500

    not needed

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,3 @@ let mix = require('laravel-mix');
 mix.js('assets/js/app.js', 'public/js/')
     .js('assets/js/renderer.js', 'public/js/')
     .sass('assets/sass/app.scss', 'public/css/')
-    // .sass('assets/sass/sniddl-mobile.scss', 'public/css/')
